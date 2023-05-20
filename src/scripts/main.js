e9d10d3ce31e3111c22934ecb32284558d5fbefa
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-05-20 13:20:45 +0900

    p5.jsの環境構築

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,14 @@
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+import p5 from "p5";
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img scripts="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img scripts="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+const sketch = (p) => {
+    p.setup = () => {
+        p.createCanvas(400, 400);
+    };
 
-// setupCounter(document.querySelector('#counter'))
+    p.draw = () => {
+        p.background(220);
+        p.ellipse(50, 50, 80, 80);
+    };
+};
+
+new p5(sketch);
