d7a36872298420d9e993a380343227ff5a4f1e3a
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-09-13 20:16:30 +0900

    線

--- coreative-coding/2023-09-13/3/index.js
+++ coreative-coding/2023-09-13/3/index.js
@@ -1,87 +1,39 @@
 // let palette = ["#C91A09", "#237841", "#42C0FB", "#FFF03A", "#FE8A18"];
-let url = 'https://coolors.co/palette/ccd5ae-e9edc9-fefae0-faedcd-d4a373';
-let palette = url.replace('https://coolors.co/', '').split('-').map(c => '#' + c);
-let num = 10;
-let size = 8;
-let step = size * 4;
-let colorArr = [];
-let sizeArr = [];
-let vmin;
+let url = "https://coolors.co/palette/ccd5ae-e9edc9-fefae0-faedcd-d4a373";
+let palette = url
+  .replace("https://coolors.co/", "")
+  .split("-")
+  .map((c) => "#" + c);
+let num = 5;
+
 
 function setup() {
   w = min(windowWidth, windowHeight);
-  createCanvas(w, w, WEBGL);
-  // debugMode();
-  ortho(-width / 2, width / 2, height / 2, -height / 2, 0, 2000);
-  for (let i = 0; i < num * num; i++) {
-    colorArr.push(random(palette));
-    sizeArr.push(size * floor(random(1, 5)));
-  }
+  createCanvas(w, w);
 }
 
 function draw() {
-  clear();
   background(255);
-  ambientLight(170); // 環境光
-  directionalLight(200, 200, 200, 1, -1, -1); // 右上奥からの光
-  camera(200,200,200);
-  rotateY(PI/3);
-  // orbitControl();
-  noStroke();
-  // LEGOを描く
-  for (let i = 0; i < num; i++) {
-    let z = i * step;
-    let pz = (i - num / 2 + 0.5) * step; // マスの中心の位置
-    for (let j = 0; j < num; j++) {
-      let px = (j - num / 2 + 0.5) * step;
-      let s = map(noise(px * 0.003+ frameCount * 0.001, pz * 0.003+ frameCount * 0.001), 0.3, 1, 0.1, 10, true); // ノイズを使ってサイズを変化させる 0.3~1を0.1~10に変換
-      if (s < 1) continue;
-      let idx = i * num + j;
-      fill(colorArr[idx]);
-      drowLEGO(step, s, { x: px, z: pz });
-    }
-  }
-}
-const randomOneToTen = (x,z) => {
-  let nx = x * 0.003 + frameCount * 0.001;
-  let nz = z * 0.003 + frameCount * 0.001;
-  let s = map(noise(nx, nz), 0.3, 1, 0.1, 10, true); // ノイズを使ってサイズを変化させる 0.3~1を0.1~10に変換
-  s = floor(s * 4) / 4;
-  return s;
-};
 
-const drowLEGO = (XZSize, heightScale, pos) => {
-  // heightはxzの大きさに対して何倍か
-  let PointYPos = (XZSize * heightScale) / 2; // ポイントのY座標　めり込ん出る
-  let BoxYPos = XZSize * heightScale;
-  let BQuad = XZSize / 4;
-  push();
-  {
-    translate(pos.x, PointYPos, pos.z);
-    _drowPoint(-BQuad, PointYPos, -BQuad, XZSize);
-    _drowPoint(BQuad, PointYPos, -BQuad, XZSize);
-    _drowPoint(BQuad, PointYPos, BQuad, XZSize);
-    _drowPoint(-BQuad, PointYPos, BQuad, XZSize);
-    _drowBox({ x: 0, y: 0, z: 0 }, { x: XZSize, y: BoxYPos, z: XZSize });
-  }
-  pop();
-};
+  textSize(20);
+  text("破線", 30, height * 0.2-15);
+  drawingContext.setLineDash([20, 20]);
+  line(30, height * 0.2, width - 30, height * 0.2);
 
-function _drowPoint(x, y, z, scaleSize) {
-  push();
-  {
-    translate(x, y, z);
-    scale(scaleSize);
-    cylinder(0.15, 0.2);
-  }
-  pop();
+  textSize(20);
+  text("一点鎖線", 30, height * 0.4-15);
+  drawingContext.setLineDash([50, 20, 5, 20]);
+  line(30, height * 0.4, width - 30, height * 0.4);
+
+  textSize(20);
+  text("実線", 30, height * 0.6-15);
+  drawingContext.setLineDash([1, 0]);
+  line(30, height * 0.6, width - 30, height * 0.6);
+
+  textSize(20);
+  text("点線", 30, height * 0.8-15);
+  strokeWeight(5);
+  strokeCap(ROUND);
+  drawingContext.setLineDash([1, 50]);
+  line(30, height * 0.8, width - 30, height * 0.8);
 }
-const _drowBox = (pos, size) => {
-  push();
-  {
-    translate(pos.x, pos.y, pos.z);
-    scale(size.x, size.y, size.z);
-    box(1);
-  }
-  pop();
-};
